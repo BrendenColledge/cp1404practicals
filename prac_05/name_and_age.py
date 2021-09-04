4f15c1fbaafdfeb6c6c231cbccabdaa7d0057e80
Brenden Colledge <brenden.colledge@my.jcu.edu.au>
2021-09-04 16:07:59 +1000

    Add dictionary practice code

---
+++ prac_05/name_and_age.py
@@ -0,0 +1,7 @@
+ages_dict = {'Brenden': 24, 'Ashley': 22, "Michael": 25}
+for name in ages_dict:
+    print("{} - {}".format(name, ages_dict[name]))
+for name, age in ages_dict.items():
+    print("{} - {}".format(name, age))
+
+
